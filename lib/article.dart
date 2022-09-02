95a1f1eddbfbebf92741e12caee6173b1c6dd4a5
sakanafuto <sakanafuto765@gmail.com>
2022-09-02 22:28:55 +0900

    feat: modelの実装

---
+++ lib/article.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_qiita_retrofit/user.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'article.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Article {
+  String id;
+  String title;
+  String url;
+
+  User user;
+
+  Article({
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.user,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) =>
+    _$ArticleFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ArticleToJson(this);
+}
